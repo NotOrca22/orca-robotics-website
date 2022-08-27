db337efd6939593f085401bce62b1191476f2324
Orca Whale <orcawhale2008@gmail.com>
2022-08-27 16:49:35 -0400

    version 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import Person from './Components/Person';
+import Header from './Components/Header';
+import { Route, BrowserRouter, Link, Routes } from "react-router-dom";
+import Members from "./Components/Members"
+import beluga from "./beluga.png";
+import { useState } from "react";
+import MainPage from './Components/MainPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  // this.state
+  const name = "Orca"
+  const [ rendered, changeRender ] = useState("")
+  const isShowing = true
+  return (<div>
+    <Header />
+    <BrowserRouter>
+    <nav>
+    <ul className="mainNav">
+    <li><Link to="/" className="homeLink" id="here" />Home</li>
+    <li><Link to="/members" className="featureLink" />Members</li>
+    <li><Link to="/notebook" className="expertLink" />Engineering Notebook</li>
+    <li><Link to="/quiz/" className="quizLink" />Outreach</li>
+    {/* <!-- <li><a href="/projects/" class="projectLink">Teams</a></li> --> */}
+  </ul>
+  </nav>
+  <Routes>
+  <Route path="/members" element= { <Members /> }></Route>
+  <Route exact path="/" element= { <MainPage />}></Route>
+  </Routes>
+  </BrowserRouter>
+  </div>
+
+    
   );
 }
 
