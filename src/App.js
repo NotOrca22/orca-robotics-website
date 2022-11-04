b9b7831531e076d50c11f3167df8553dee9b60af
Orca Whale <orcawhale2008@gmail.com>
2022-11-04 16:30:43 -0400

    stuff

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Members from "./Components/Members"
 import beluga from "./beluga.png";
 import { useState } from "react";
 import MainPage from './Components/MainPage';
+import LoginForm from './Components/LoginForm';
 
 const App = () => {
   // this.state
@@ -22,12 +23,15 @@ const App = () => {
     <li className="homeLink"><Link to="/members">Members</Link></li>
     <li><Link to="/notebook">Engineering Notebook</Link></li>
     <li><Link to="/quiz/">Outreach</Link></li>
+    <li><Link to="/login">Login</Link></li>
+    <li><Link to="/login">Register</Link></li>
     {/* <!-- <li><a href="/projects/" class="projectLink">Teams</a></li> --> */}
   </ul>
   </nav>
   <Routes>
   <Route path="/members" element= { <Members /> }></Route>
   <Route exact path="/" element= { <MainPage />}></Route>
+  <Route exact path="/login" element= { <LoginForm />}></Route>
   </Routes>
   </BrowserRouter>
   </div>
